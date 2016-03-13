34c096b5c513826fc593a78384d6e0bb7feda618
Eric Crescioni <ecam900@gmail.com>
2016-03-13 23:17:55 +0000

    Fix modal

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,5 +10,5 @@ CarrierWave.configure do |config|
       access_key_id:     ENV["AWS_ACCESS_KEY"],
       secret_access_key: ENV["AWS_SECRET_KEY"],
       region:            ENV["AWS_REGION"]
-  }               # required
+  }
 end
